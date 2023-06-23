8522ba63f4b619fa7f9e404ef15b8668b2f1e17d
Steven Bustillo <sb.sys.dev@gmail.com>
2023-06-23 07:46:38 -0600

    :bug: - updated loader & notification with portal

--- src/contexts/core/loader/Loader.provider.tsx
+++ src/contexts/core/loader/Loader.provider.tsx
@@ -1,5 +1,6 @@
 // react
 import { memo } from 'react';
+import { createPortal } from 'react-dom';
 // hooks
 import { useLoader } from './useLoader.hook';
 // components
@@ -8,18 +9,17 @@ import Loader from './Loader';
 const LoaderProvider = memo(() => {
     const { isLoading } = useLoader();
 
-    return (
-        <>
-            {isLoading && (
-                <div className="fixed inset-0 p-4 z-[200] flex flex-col justify-center items-center overflow-hidden">
-                    <div className="absolute inset-0 bg-dark-900 bg-opacity-75" />
+    if (!isLoading) return null;
 
-                    <span className="absolute flex flex-col justify-center items-center w-24 overflow-hidden text-primary-500">
-                        <Loader />
-                    </span>
-                </div>
-            )}
-        </>
+    return createPortal(
+        <div className="fixed inset-0 p-4 z-[200] flex flex-col justify-center items-center overflow-hidden">
+            <div className="absolute inset-0 bg-dark-900 bg-opacity-75" />
+
+            <span className="absolute flex flex-col justify-center items-center w-24 overflow-hidden text-primary-500">
+                <Loader />
+            </span>
+        </div>,
+        document.getElementById('loader') as HTMLElement
     );
 });
 
--- src/contexts/core/notification/Notification.provider.tsx
+++ src/contexts/core/notification/Notification.provider.tsx
@@ -1,5 +1,6 @@
 // react
 import { memo } from 'react';
+import { createPortal } from 'react-dom';
 // hooks
 import { useNotification } from './useNotification.hook';
 // layouts
@@ -10,23 +11,22 @@ import Notification from './Notification';
 const NotificationProvider = memo(() => {
     const { notificationList } = useNotification();
 
-    return (
-        <>
-            {notificationList.length > 0 && (
-                <ScrollLayout
-                    className="fixed top-0 right-0 z-[300] max-h-full max-w-full p-4 pointer-events-none"
-                    classNameContent="pointer-events-auto"
-                    direction="top-bottom">
-                    <ul className="flex flex-col gap-2">
-                        {notificationList.map((notification, index) => (
-                            <li key={index}>
-                                <Notification {...notification} />
-                            </li>
-                        ))}
-                    </ul>
-                </ScrollLayout>
-            )}
-        </>
+    if (notificationList.length === 0) return null;
+
+    return createPortal(
+        <ScrollLayout
+            className="fixed top-0 right-0 z-[300] max-h-full max-w-full p-4 pointer-events-none"
+            classNameContent="pointer-events-auto"
+            direction="top-bottom">
+            <ul className="flex flex-col gap-2">
+                {notificationList.map((notification, index) => (
+                    <li key={index}>
+                        <Notification {...notification} />
+                    </li>
+                ))}
+            </ul>
+        </ScrollLayout>,
+        document.getElementById('notify') as HTMLElement
     );
 });
 
